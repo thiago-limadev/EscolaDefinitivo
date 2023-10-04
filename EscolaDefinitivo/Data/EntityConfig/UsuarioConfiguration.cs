3aecea4fb1df430ac1b4b8ed5f2660f0964839fa
Thiago Lima <thiago.lima951@gmail.com>
2023-10-04 10:26:16 -0300

    Ajuste em configuração de senha padrão de usuário Admin

--- EscolaDefinitivo/Data/EntityConfig/UsuarioConfiguration.cs
+++ EscolaDefinitivo/Data/EntityConfig/UsuarioConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using EscolaDefinitivo.Models;
+﻿using EscolaDefinitivo.Helpper;
+using EscolaDefinitivo.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -10,6 +11,7 @@ namespace EscolaDefinitivo.Data.EntityConfig
         {
             var admin = new Usuario("Ademiro", "Admin", "Admin@admin.com", "Admin123", Enums.PerfilEnum.Admin);
             admin.Id = 1;
+            admin.SetSenhaHash();
 
             builder
                 .HasData(admin);
